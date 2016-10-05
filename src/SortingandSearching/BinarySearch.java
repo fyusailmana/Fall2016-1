d6f7c26edb0e6649de80f81ccd5b296c19887d56
Fahmi Yusa Ilmana <fhmi_yusa@yahoo.co.id>
2016-10-05 14:14:10 -0500

    Update BinarySearch.java

--- src/SortingandSearching/BinarySearch.java
+++ src/SortingandSearching/BinarySearch.java
@@ -3,6 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package SortingandSearching;
 
 /**
@@ -11,4 +12,27 @@ package SortingandSearching;
  */
 public class BinarySearch {
     
+    int[]data;
+    int size;
+    
+    public boolean binarySearch(int key) {
+    int min = 0;
+    int max = size - 1;
+        
+    while(max >= min)
+    
+{
+    int middle = (min + max) / 2;
+    if(data[middle] == key){
+        return true;
+    }
+    
+    if(data[middle] < key) {
+        min = middle + 1;
+    }
+    if (data[middle] > key) {
+        max = middle - 1;
+    }
 }
+    return false;
+}}
